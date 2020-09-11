47e094f666ccd387973fab20826bb621af4565ec
Minsoo Kim <minsooerickim@gmail.com>
2020-09-11 15:56:18 -0700

    Finished Tic Tac Toe

--- main.cpp
+++ main.cpp
@@ -1,58 +1,92 @@
 #include <iostream>
 using namespace std;
 
-int const width = 9, height = 6;
+char matrix[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char userInput, player = 'X';
+bool gameOver = true, repeat = true;
+int turnCount = 0;
 
 void draw();
+void input();
+void togglePlayer();
+void winner();
 
 int main() {
+	cout << "Tic Tac Toe" << endl;
 	draw();
+	while (gameOver) {
+		input();
+		draw();
+		winner();
+		togglePlayer();
+	}
 }
-
-void Setup() {
-
-}
-
 void draw() {
-	system("cls");
-	//top wall	
-	for (int i = 0; i <= width; i++) {
-		cout << "*";
+	cout << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
 	}
 	cout << endl;
+}
+void input() {
+	repeat = true;
+	while (repeat) {
+		cout << "Enter the number in the field: ";
+		cin >> userInput;
 
-	int hozLineInc = height / 3;
-	int vertLineInc = width / 3;
-
-	for (int j = 1; j < height; j++) {
-		for (int i = 0; i <= width; i++) {
-			if (i == 0) {
-				if (j == 1 && i == 0) {
-					cout << "* 1 * 2 * 3 *" << endl;
-					cout << "*************" << endl;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (userInput == matrix[i][j]) {
+					matrix[i][j] = player;
+					repeat = false;
+					turnCount += 1;
 				}
-				else if (j == 3 && i == 0) {
-					cout << "* 4 * 5 * 6";
-				}
-				else if (j == 5 && i == 0) {
-					cout << "* 7 * 8 * 9";
-				}
-
 			}
-			cout << endl;
 		}
-		//bottom wall
-		for (int i = 0; i <= width; i++) {
-			cout << "*";
-		}
+		if (repeat == true)
+			cout << "\nThis field is already taken!\n" << endl;
 	}
 }
-
-
-void input() {
-
+void togglePlayer() {
+	if (player == 'X')
+		player = 'O';
+	else
+		player = 'X';
 }
-
-void logic() {
-
+void winner() {
+	//check rows
+	for (int i = 0; i < 3; i++) {
+		if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2]) {
+			gameOver = false;
+			cout << "Game Over! " << matrix[i][0] << " Wins!";
+			break;
+		}
+	}
+	//check columns
+	for (int j = 0; j < 3; j++) {
+		if (matrix[0][j] == matrix[1][j] && matrix[1][j] == matrix[2][j]) {
+			gameOver = false;
+			cout << "Game Over! " << matrix[0][j] << " Wins!";
+			break;
+		}
+	}
+	//diagonals
+	if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) {
+		gameOver = false;
+		cout << "Game Over! "<< matrix[0][0] << " Wins!";
+	}
+	if (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) {
+		gameOver = false;
+		cout << "Game Over! " << matrix[0][2] << " Wins!";
+	}
+	
+	if (turnCount == 9) {
+		if (gameOver) {
+			cout << "It is a tie! Nobody wins ahahah!";
+			gameOver = false;
+		}
+	}
 }
